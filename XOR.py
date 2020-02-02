7b379e49c2828f0e0c1a4168212c02999f554a17
Kahzerx <kahzer123@gmail.com>
2020-02-02 16:14:51 +0100

    aprende a hacer una XOR

--- XOR.py
+++ XOR.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 model = kh.GenerateDense([4, 8, 1], ["sigmoid", "tanh", "sigmoid"], "mse", "adam")
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 Y = np.array([[0], [1], [1], [0]])
+
 plt.plot(kh.FitModel(model, X, Y, 0.9))
 plt.show()
+
 plt.plot(model.predict_proba(X))
 plt.show()
